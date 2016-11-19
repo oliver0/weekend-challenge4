c5ab4171da7e1a22b49d5957381072be0d7c4ced
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-19 16:26:13 -0600

    Make insert query to database. Worked correctly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     var task = {};
     task.task_name = userTask;
     task.complete = "No";
-    console.log(task);
+
     $.ajax({
       type: 'POST',
       url: '/tasks',
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,10 +2,31 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/todo_list';
-console.log('Made it to tasks.js');
+
 router.post('/', function(req, res){
-  console.log('REQ: ', req.body);
-  res.sendStatus(201);
+  var task = req.body;
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query(
+      'INSERT INTO tasks (task_name, complete) ' +
+      'VALUES ($1, $2)',
+      [task.task_name, task.complete],
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.log('insert query error: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+
+  });
 });
 
 module.exports = router;
