7c072c5b844194064f0fbd8e7a7a006ab9020947
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-19 15:58:40 -0600

    Change submit to add. Create addTask function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-
-  $('#submit').on('click', function(){
-    console.log($('#task').val());
+  // listener for add button. When clicked addTask function is run, using the task in input field as parameter
+  $('#add').on('click', function(){
+    var userTask = $('#task').val();
+    addTask(userTask);
   });
+
+  // takes in task as a parameter. Makes task object with task_name and complete properties. Makes ajax post to
+  // tasks.routes, sending task object as data.
+  function addTask(userTask){
+    var task = {};
+    task.task_name = userTask;
+    task.complete = "No";
+    console.log(task);
+  }
+
+
 });
