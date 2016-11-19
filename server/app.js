fcd8a427b0f3e8e8664ce57fb88e39e4fffcb7cd
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-19 15:06:15 -0600

    Initial Commit - Project connected

---
+++ server/app.js
@@ -0,0 +1,25 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var tasks = require('./routes/tasks');
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// tasks route
+app.use('/task', tasks);
+
+// Catchall route
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('./public/views/index.html'));
+});
+
+app.use(express.static('./public'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ', app.get('port'));
+
+});
---
+++ server/routes/tasks.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
+
+module.exports = router;
