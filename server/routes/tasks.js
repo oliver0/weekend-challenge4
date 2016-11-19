2070ff395cbb50e613e217dc991a5dd23d4aebc4
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-19 15:44:50 -0600

    Create container + input and add basic styles

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/sigma';
+var connectionString = 'postgres://localhost:5432/todo_list';
 
 module.exports = router;
